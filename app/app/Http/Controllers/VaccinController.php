9b9fbac47be04c55bd264116606c46e143392e94
thomas.chatel <thomas.chatel.acversailles@gmail.com>
2021-04-15 11:12:11 +0200

    vaccin crud rest api

---
+++ app/app/Http/Controllers/VaccinController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vaccins;
+use Illuminate\Http\Request;
+
+class VaccinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Vaccins::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $vaccin = new Vaccins([
+            'code_region' => $request->code_region,
+            'nom_reg' => $request->nom_reg,
+            'type_de_vaccin' => $request->type_de_vaccin,
+            'nb_ucd' => $request->nb_ucd,
+            'nb_doses' => $request->nb_doses,
+            'date' => $request->date,
+        ]);
+        return Vaccins::create($vaccin->toArray());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vaccins $vaccin)
+    {
+        return $vaccin;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vaccins $vaccin)
+    {
+        $vaccin->code_region = $request->code_region;
+        $vaccin->nom_reg = $request->nom_reg;
+        $vaccin->type_de_vaccin = $request->type_de_vaccin;
+        $vaccin->nb_ucd = $request->nb_ucd;
+        $vaccin->nb_doses = $request->nb_doses;
+        $vaccin->date = $request->date;
+
+        $vaccin->update();
+        return $vaccin;
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Vaccins::destroy($id);
+    }
+}
